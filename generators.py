411997a28eeabe4340fd62cdecfbc274066daf1b
Kian Hunziker <kian.hunziker@unibas.ch>
2023-10-19 15:18:35 +0200

    add naive implementation of unconditional generator by removing label embedding

--- generators.py
+++ generators.py
@@ -10,6 +10,12 @@ from groupy.gconv.pytorch_gconv import P4ConvZ2, P4ConvP4
 
 class Generator(nn.Module):
     def __init__(self, n_classes: int = 10, gen_arch: str = 'z2_rot_mnist', latent_dim: int = 64):
+        """
+        Constructor for a generator
+        :param n_classes: number of classes for conditional use
+        :param gen_arch: Architecture type. One of {'z2_rot_mnist', 'p4_rot_mnist', 'z2_rot_mnist_no_label'}
+        :param latent_dim: Dimensionality of latent noise input
+        """
         super(Generator, self).__init__()
         self.gen_arch = gen_arch
         self.channel_scale_factor = 1
@@ -20,13 +26,17 @@ class Generator(nn.Module):
         self.proj_shape = (128, 7, 7)
         self.label_emb_dim = 64
 
-        self.label_projection_layer = nn.Linear(in_features=n_classes,
-                                                out_features=self.label_emb_dim,
-                                                bias=False)
-        self.projected_noise_and_classes = SN(nn.Linear(in_features=latent_dim + self.label_emb_dim,
-                                                        out_features=self.proj_dim))
+        if gen_arch == 'z2_rot_mnist_no_label':
+            self.projected_noise_layer = SN(nn.Linear(in_features=latent_dim,
+                                                      out_features=self.proj_dim))
+        else:
+            self.label_projection_layer = nn.Linear(in_features=n_classes,
+                                                    out_features=self.label_emb_dim,
+                                                    bias=False)
+            self.projected_noise_and_classes = SN(nn.Linear(in_features=latent_dim + self.label_emb_dim,
+                                                            out_features=self.proj_dim))
 
-        if self.gen_arch == 'z2_rot_mnist':
+        if self.gen_arch == 'z2_rot_mnist' or self.gen_arch == 'z2_rot_mnist_no_label':
             self.conv1 = SN(nn.Conv2d(in_channels=128,
                                       out_channels=512,
                                       kernel_size=3,
@@ -97,8 +107,8 @@ class Generator(nn.Module):
                                      padding=1,
                                      bias=False))
 
-    def forward(self, latent_noise: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
-        '''
+    def forward(self, latent_noise: torch.Tensor, label: torch.Tensor = None) -> torch.Tensor:
+        """
         cla: concatenated vector [latent_noise, embedded_labels]
         gen: reshaped input to convolutional layers: [batch_size, (proj_shape)]
         for Z2 gen has shape [batch_size, 128, 7, 7]
@@ -106,14 +116,22 @@ class Generator(nn.Module):
         :param latent_noise: noise to synthesize images from [batch_size, latent_dim]
         :param label: class labels one hot encoded [batch_size, n_classes]
         :return: synthesized images [batch_size, n_channels, height, width]
-        '''
-        label_projection = self.label_projection_layer(label)
-        cla = torch.cat((latent_noise, label_projection), dim=1)
-        cla = self.projected_noise_and_classes(cla)
-        # TODO make reshape nicer
-        gen = cla.reshape(tuple([-1]) + self.proj_shape)
+        """
 
-        if self.gen_arch == 'z2_rot_mnist':
+        if self.gen_arch == 'z2_rot_mnist_no_label':
+            # cla: projected noise, as a vector of length proj_dim
+            cla = self.projected_noise_layer(latent_noise)
+            # gen: input for convolutions, shape: proj_shape
+            gen = cla.reshape(tuple([-1]) + self.proj_shape)
+
+        else:
+            label_projection = self.label_projection_layer(label)
+            cla = torch.cat((latent_noise, label_projection), dim=1)
+            cla = self.projected_noise_and_classes(cla)
+            # TODO make reshape nicer
+            gen = cla.reshape(tuple([-1]) + self.proj_shape)
+
+        if 'z2_rot_mnist' in self.gen_arch:
             # now cla should be: [batch_size,128, 7, 7]
             fea = self.conv1(gen)
             fea = F.relu(fea)
@@ -134,6 +152,7 @@ class Generator(nn.Module):
             fea = F.relu(fea)
             fea = self.conv4(fea)
             # [batch_size, 1, 28, 28
+
         elif self.gen_arch == 'p4_rot_mnist':
             fea = F.relu(self.conv1(gen))
 
@@ -186,4 +205,4 @@ def generator_initialisation_test():
     print('great success!')
 
 # generator_initialisation_test()
-#generator_debug_test('p4_rot_mnist')
+# generator_debug_test('p4_rot_mnist')
