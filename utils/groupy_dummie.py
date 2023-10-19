7966fe1a1615dc3cf034c976a544e4d2798eca44
Kian Hunziker <kian.hunziker@unibas.ch>
2023-10-19 17:57:24 +0200

    add dummie functions to replace groupy package on google colab

---
+++ utils/groupy_dummie.py
@@ -0,0 +1,21 @@
+"""
+Dummie functions to import when running without installed GrouPy package
+"""
+
+
+def P4ConvZ2(in_channels,
+             out_channels,
+             kernel_size,
+             stride=1,
+             padding=1,
+             bias=True):
+    return None
+
+
+def P4ConvP4(in_channels,
+             out_channels,
+             kernel_size,
+             stride=1,
+             padding=1,
+             bias=True):
+    return None
