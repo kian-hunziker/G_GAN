d91dfd064a8cdfc01ddf801f1bc81d8fe0a59fc9
Kian Hunziker <kian.hunziker@unibas.ch>
2023-10-20 16:44:48 +0200

    add blocks for vanilla WGAN architecture

--- blocks.py
+++ blocks.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.utils import spectral_norm as SN
 import torch.nn.functional as F
+
 try:
     from groupy.gconv.pytorch_gconv import P4ConvZ2, P4ConvP4
 except ImportError:
@@ -73,7 +74,7 @@ class DiscBlock(nn.Module):
                                         out_channels=out_features,
                                         kernel_size=kernel_size,
                                         stride=1,
-                                        padding=kernel_size//2,
+                                        padding=kernel_size // 2,
                                         bias=True))
             elif self.h_input == 'C4' and h_output == 'C4':
                 self.conv = SN(P4ConvP4(in_channels=in_features,
@@ -111,3 +112,39 @@ class DiscBlock(nn.Module):
                 elif self.pool == 'avg':
                     out = F.avg_pool2d(out, kernel_size=2)
         return out
+
+
+class GenBlockDCGAN(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(GenBlockDCGAN, self).__init__()
+        self.block = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=in_channels,
+                               out_channels=out_channels,
+                               kernel_size=kernel_size,
+                               stride=stride,
+                               padding=padding,
+                               bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class DiscBlockDCGAN(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(DiscBlockDCGAN, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels,
+                      out_channels=out_channels,
+                      kernel_size=kernel_size,
+                      stride=stride,
+                      padding=padding,
+                      bias=False),
+            nn.InstanceNorm2d(out_channels, affine=True),
+            nn.LeakyReLU(negative_slope=0.2)
+        )
+
+    def forward(self, x):
+        return self.block(x)
