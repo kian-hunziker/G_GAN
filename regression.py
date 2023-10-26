5b2ae8c78d75698ce2a987877c44f464c96c54bc
Kian Hunziker <kian.hunziker@unibas.ch>
2023-10-26 13:27:33 +0200

    adapt script to run multiple regressions and compute peak SNR values

--- regression.py
+++ regression.py
@@ -4,6 +4,8 @@ import torch
 import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from torchmetrics.functional.image import peak_signal_noise_ratio
 
 import random
 from tqdm import tqdm
@@ -11,22 +13,36 @@ from tqdm import tqdm
 from utils.dataLoaders import get_rotated_mnist_dataloader
 from utils.checkpoints import load_checkpoint
 
+
+def plot_comparison(target: torch.Tensor, approx: torch.Tensor, title: str):
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(target.detach().cpu().numpy(), cmap='gray')
+    ax[0].set_title('Target')
+    ax[1].imshow(approx.detach().cpu().numpy(), cmap='gray')
+    ax[1].set_title('Approximation')
+    plt.suptitle(title)
+    plt.show()
+
 device = 'cpu'
 # fix random seed for target selection. Torch seed is not fixed.
 random.seed(2)
 
 # for conditional generators we need to specify which class we're looking at
-class_to_search = 8
+class_to_search = 0
 
 # Hyperparameters
 lr = 1e-2
 weight_decay = 1.0
-n_iterations = 1000
-n_training_examples = 12000
+n_iterations = 300
+img_size = 64
+latent_dim = 100
 
-step_for_plot = 200
+n_regressions = 500
 
-gen, _ = load_checkpoint('trained_models/z2_rot_mnist/2023-10-19 15:42:33/checkpoint_20000', device=device)
+step_for_plot = n_iterations
+plot_individual_loss_and_mag = False
+
+gen, _ = load_checkpoint('trained_models/vanilla/2023-10-20 13:07:54/checkpoint_20000', device=device)
 '''
 gen = Generator()
 gen.load_state_dict(
@@ -36,76 +52,116 @@ gen.load_state_dict(
 gen.eval()
 '''
 
-#latent_noise = torch.zeros(1, 64, requires_grad=True, dtype=torch.float, device=device)
-latent_noise = torch.randn(1, 64, requires_grad=True, dtype=torch.float, device=device)
-label = torch.zeros(1, 10, device=device)
-label[0, class_to_search] = 1
+latent_noise_matrix = torch.zeros(n_regressions, latent_dim, dtype=torch.float)
 
 project_root = os.path.dirname(os.path.abspath(__file__))
-dataset, _ = get_rotated_mnist_dataloader(root=project_root,
-                                          batch_size=64,
-                                          shuffle=True,
-                                          one_hot_encode=True,
-                                          num_examples=60000,
-                                          num_rotations=0)
+test_dataset, _ = get_rotated_mnist_dataloader(root=project_root,
+                                               batch_size=64,
+                                               shuffle=True,
+                                               one_hot_encode=True,
+                                               num_examples=10000,
+                                               num_rotations=0,
+                                               train=False)
 
-indices_of_target_class = np.where(dataset.targets[n_training_examples:] == class_to_search)[0]
-target_idx = indices_of_target_class[random.randint(0, len(indices_of_target_class) - 1)] + n_training_examples
-target = dataset.data[target_idx]
+indices_of_target_class = np.where(test_dataset.targets == class_to_search)[0]
+targets = test_dataset.data[indices_of_target_class][:n_regressions]
+
+all_targets = torch.zeros(n_regressions, 1, img_size, img_size)
 
 transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
-    )
-target = transform(target)
+    [transforms.ToTensor(),
+     transforms.Resize(img_size),
+     # transforms.RandomAffine(degrees=180, translate=(0.1, 0.1), scale=(0.8, 1), shear=[10,10,10,10]),
+     transforms.Normalize((0.5,), (0.5,))]
+)
 
-optim = torch.optim.Adam(params=[latent_noise], lr=lr, weight_decay=weight_decay)
-criterion = torch.nn.MSELoss(reduction='sum')
+prog_bar = tqdm(total=n_iterations * n_regressions)
+prog_bar.set_description(f'Total of {n_regressions} regressions with {n_iterations} iterations each')
 
-losses = []
-magnitudes = []
-prog_bar = tqdm(total=n_iterations)
+for target_idx in range(n_regressions):
+    latent_noise = torch.zeros(1, latent_dim, requires_grad=True, dtype=torch.float, device=device)
+    label = torch.zeros(1, 10, device=device)
+    label[0, class_to_search] = 1
+    target = targets[target_idx]
+    target = transform(target).squeeze()
+    target = target + torch.randn((img_size, img_size)) * 0.0
+    target = target.to(device)
 
-for i in range(n_iterations):
-    optim.zero_grad()
+    optim = torch.optim.Adam(params=[latent_noise], lr=lr, weight_decay=weight_decay)
+    criterion = torch.nn.MSELoss(reduction='sum')
 
-    approx = gen(latent_noise, label)[0]
-    loss = criterion(approx, target)
+    losses = []
+    magnitudes = []
 
-    losses.append(loss.detach().cpu().numpy())
-    mag = torch.linalg.vector_norm(latent_noise)
-    magnitudes.append(mag.detach().cpu().numpy())
+    for i in range(n_iterations):
+        optim.zero_grad()
 
-    loss.backward()
-    optim.step()
+        approx = gen(latent_noise, label).squeeze()
+        loss = criterion(approx, target)
 
-    if i % step_for_plot == 0:
+        losses.append(loss.detach().cpu().numpy())
+        mag = torch.linalg.vector_norm(latent_noise)
+        magnitudes.append(mag.detach().cpu().numpy())
+
+        loss.backward()
+        optim.step()
+
+        if i > 0 and i % step_for_plot == 0:
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(target.detach().cpu().numpy(), cmap='gray')
+            ax[0].set_title('Target')
+            ax[1].imshow(approx.detach().cpu().numpy(), cmap='gray')
+            ax[1].set_title('Approximation')
+            plt.suptitle(f'Iteration {i} / {n_iterations}')
+            plt.tight_layout()
+            plt.show()
+
+        prog_bar.update(1)
+
+    latent_noise_matrix[target_idx] = latent_noise.detach()
+    all_targets[target_idx, 0] = target
+
+    if plot_individual_loss_and_mag is True:
         fig, ax = plt.subplots(1, 2)
-        ax[0].imshow(target.detach().cpu().numpy()[0], cmap='gray')
+        ax[0].imshow(target.detach().cpu().numpy(), cmap='gray')
         ax[0].set_title('Target')
-        ax[1].imshow(approx.detach().cpu().numpy()[0], cmap='gray')
+        ax[1].imshow(approx.detach().cpu().numpy(), cmap='gray')
         ax[1].set_title('Approximation')
-        plt.suptitle(f'Iteration {i} / {n_iterations}')
-        plt.tight_layout()
+        plt.suptitle(f'Final approximation')
         plt.show()
 
-    prog_bar.update(1)
+        plt.plot(losses)
+        plt.title('Loss over iterations')
+        plt.show()
 
+        plt.plot(magnitudes)
+        plt.title('Magnitude of latent noise over iterations')
+        plt.show()
+
+#print(f'final latent vector: \n{latent_noise}')
 prog_bar.close()
 
-fig, ax = plt.subplots(1, 2)
-ax[0].imshow(target.detach().cpu().numpy()[0], cmap='gray')
-ax[0].set_title('Target')
-ax[1].imshow(approx.detach().cpu().numpy()[0], cmap='gray')
-ax[1].set_title('Approximation')
-plt.suptitle(f'Final approximation')
+all_predictions = gen(latent_noise_matrix, None)
+snrs = peak_signal_noise_ratio(preds=all_predictions,
+                               target=all_targets,
+                               reduction='none',
+                               dim=(1, 2, 3),
+                               data_range=(-1, 1))
+
+snrs_numpy = snrs.detach().cpu().numpy()
+
+plt.plot(np.arange(n_regressions, dtype=int), snrs_numpy, 'o')
+plt.xticks(np.arange(n_regressions, dtype=int))
+plt.title('Peak Signal-To-Noise_Ratios')
 plt.show()
 
-plt.plot(losses)
-plt.title('Loss over iterations')
+sns.histplot(snrs_numpy)
+plt.title(f'Histogram of PSNRs, total number of examples: {len(snrs_numpy)}')
+plt.xlabel('PSNR values')
 plt.show()
 
-plt.plot(magnitudes)
-plt.title('Magnitude of latent noise over iterations')
-plt.show()
+idx_worst_snr = torch.argmin(snrs).item()
+idx_best_snr = torch.argmax(snrs).item()
 
-print(f'final latent vector: \n{latent_noise}')
+plot_comparison(all_targets[idx_worst_snr, 0], all_predictions[idx_worst_snr, 0], title=f'Worst approximation. SNR: {snrs[idx_worst_snr].item():.2f}')
+plot_comparison(all_targets[idx_best_snr, 0], all_predictions[idx_best_snr, 0], title=f'Best approximation. SNR: {snrs[idx_best_snr].item():.2f}')
