2b48713d8c4665c34835ae0576c1eace9a799044
Kian Hunziker <kian.hunziker@unibas.ch>
2023-12-08 12:44:08 +0100

    add grid_sample() to PatchedImage dataset to generate random patches not confined to the actual pixel grid

--- utils/lodopab_dataset.py
+++ utils/lodopab_dataset.py
@@ -1,8 +1,11 @@
-import h5py
+import numpy as np
 import torch
+import torch.nn.functional as F
 import torchvision.transforms as transforms
-from patchify import patchify
 from torch.utils.data import Dataset
+
+import h5py
+from patchify import patchify
 
 from utils.siren_utils import get_mgrid
 
@@ -64,33 +67,81 @@ class PatchedImage(Dataset):
     """
     Dataset to load and patchify a single image.
     """
-    def __init__(self, img_path: str, img_idx: int = 0, patch_size: int = 8):
+    def __init__(self, img_path: str, img_idx: int = 0, patch_size: int = 8,
+                 noise_strength: int = 0, use_grid_sample: bool = False):
         """
-        Initialize dataset
+        Initialize dataset.
         :param img_path: Path to the h5 dataset
         :param img_idx: Index of the image to load
         :param patch_size: Size of the patches to generate. Patches are quadratic
+        :param use_grid_sample: If enabled, the get_item method generates a random 2D coordinate in the range
+        [-1, 1] for both x and y. Then affine_grid() and grid_sample() are used to sample a patch from the image.
         """
         super().__init__()
         # load single image
         f = h5py.File(img_path)
         img = f['data'][img_idx]
 
-        # generate patches
-        self.patches = patchify(img, patch_size=(patch_size, patch_size), step=1)
+        assert img.shape[-1] == img.shape[-2]
 
-        # generate 2D coords in range [-1, 1]
-        self.coords = get_mgrid(self.patches.shape[0], dim=2)
-        # reverse order of y-coords. The top left corner should have coords [-1, 1]
-        temp_x = 1.0 * self.coords[:, 0]
-        self.coords[:, 0] = 1.0 * self.coords[:, 1]
-        self.coords[:, 1] = 1.0 * temp_x
-        self.coords[:, 1] = -1.0 * self.coords[:, 1]
+        # add noise to image
+        img = img + noise_strength * (np.random.randn(img.shape[-2], img.shape[-1]) + 0.5)
+        self.img = img
+
+        self.use_grid_sample = use_grid_sample
+        self.patch_size = patch_size
+        self.num_patches = (self.img.shape[-2] - self.patch_size + 1) * (self.img.shape[-1] - self.patch_size + 1)
+
+        if use_grid_sample is True:
+            self.img = torch.from_numpy(img).unsqueeze(0).unsqueeze(0).type(torch.float)
+            coord_range_x = (img.shape[-1] - patch_size) / img.shape[-1]
+            coord_range_y = (img.shape[-2] - patch_size) / img.shape[-2]
+            self.coord_range = torch.tensor([coord_range_x, coord_range_y])
+            scale_width = patch_size / img.shape[-1]
+            scale_height = patch_size / img.shape[-2]
+            self.scale_matrix = torch.tensor([[scale_width, 0], [0, scale_height]])
+        else:
+            # generate patches
+            self.patches = patchify(img, patch_size=(patch_size, patch_size), step=1)
+
+            # generate 2D coords in range [-1, 1]
+            self.coords = get_mgrid(self.patches.shape[0], dim=2)
+            # reverse order of y-coords. The top left corner should have coords [-1, 1]
+            temp_x = 1.0 * self.coords[:, 0]
+            self.coords[:, 0] = 1.0 * self.coords[:, 1]
+            self.coords[:, 1] = 1.0 * temp_x
+            self.coords[:, 1] = -1.0 * self.coords[:, 1]
+
+    def grid_sample_patch(self, coord: torch.Tensor):
+        coord_affine = coord * self.coord_range
+        coord_affine[1] = -1.0 * coord_affine[1]
+        theta = torch.hstack((self.scale_matrix, coord_affine.unsqueeze(-1))).unsqueeze(0)
+        aff_grid = F.affine_grid(theta, [1, 1, self.patch_size, self.patch_size])
+        sampled_patch = F.grid_sample(self.img, aff_grid, align_corners=False)
+        return sampled_patch[0]
+
+    def get_all_coords_and_patches(self):
+        coords = get_mgrid(self.img.shape[-1] - self.patch_size + 1, 2)
+        temp_x = 1.0 * coords[:, 0]
+        coords[:, 0] = 1.0 * coords[:, 1]
+        coords[:, 1] = 1.0 * temp_x
+        coords[:, 1] = -1.0 * coords[:, 1]
+        if self.use_grid_sample is True:
+            im = self.img.squeeze().numpy()
+        else:
+            im = self.img
+        patches = patchify(im, patch_size=(self.patch_size, self.patch_size), step=1)
+        return coords, patches
 
     def __len__(self):
-        return self.patches.shape[0] * self.patches.shape[1]
+        return self.num_patches
 
     def __getitem__(self, idx):
-        x_idx = idx // self.patches.shape[0]
-        y_idx = idx % self.patches.shape[1]
-        return self.coords[idx], self.patches[x_idx, y_idx]
+        if self.use_grid_sample is True:
+            coord = 2.0 * (torch.rand(2) - 0.5)
+            patch = self.grid_sample_patch(coord)
+            return coord, patch
+        else:
+            x_idx = idx // self.patches.shape[0]
+            y_idx = idx % self.patches.shape[1]
+            return self.coords[idx], self.patches[x_idx, y_idx]
