ae59967fcc87afb3b7ffd0dd7aebdf9ffb38f597
Kian Hunziker <kian.hunziker@unibas.ch>
2024-01-26 11:11:12 +0100

    add simple fully connected mlp

--- siren.py
+++ siren.py
@@ -68,3 +68,28 @@ class Siren(nn.Module):
         coords = coords.clone().detach().requires_grad_(True)  # allows to take derivative w.r.t. input
         output = self.net(coords)
         return output, coords
+
+
+class SimpleMLP(nn.Module):
+    def __init__(self, in_features, hidden_features, hidden_layers, out_features, outermost_linear=False,
+                 first_omega_0=30, hidden_omega_0=30.):
+        super().__init__()
+        self.net = []
+        self.net.append(nn.Linear(in_features, hidden_features))
+        self.net.append(nn.ReLU())
+
+        for i in range(hidden_layers):
+            self.net.append(nn.Linear(hidden_features, hidden_features))
+            self.net.append(nn.ReLU())
+
+        self.net.append(nn.Linear(hidden_features, out_features))
+
+        if not outermost_linear:
+            self.net.append(nn.ReLU())
+
+        self.net = nn.Sequential(*self.net)
+
+    def forward(self, coords):
+        coords = coords.clone().detach().requires_grad_(True)  # allows to take derivative w.r.t. input
+        output = self.net(coords)
+        return output, coords
